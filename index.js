2e3d2769fad2eff9539b6572c0104081db7212b5
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-07-12 08:59:45 +0300

    Updates

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 // Write your solution in this file!
-var customerName = 'bob';
-
-function upperCaseCustomerName() {
-    return customerName.toUpperCase();
-  }  
-  console.log(upperCaseCustomerName()); // Output: BOB
-
-console.log(uppercaseModifier(customerName)); // Output: BOB
-function setBestCustomer() {
-    window.bestCustomer = 'not bob';
-  }
-
-function overwriteBestCustomer() {
-  bestCustomer = 'maybe bob';
-}
-
-const leastFavoriteCustomer = 'alice';
-
-function changeLeastFavoriteCustomer() {
-    const leastFavoriteCustomer = 'john'; // declare the variable with const
-    leastFavoriteCustomer = 'charlie'; // This will throw an error: "Assignment to constant variable."
-  }
-  
-  try {
-    changeLeastFavoriteCustomer();
-  } catch (error) {
-    console.error(error); // Output: TypeError: Assignment to constant variable.
-  }
+var customerName='bob';
+function  upperCaseCustomerName(){ return customerName = customerName.toUpperCase();}
+function setBestCustomer(){return bestCustomer='not bob';}
+function overwriteBestCustomer(){return bestCustomer='maybe bob';}
+const leastFavoriteCustomer=`james`
+function changeLeastFavoriteCustomer(){return leastFavoriteCustomer=`ted`;}
